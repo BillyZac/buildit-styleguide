57f6f947e545b91aa69e38fabc3f3ff4b72ce96a
Zachary Smith <william.smith1@wipro.com>
2017-03-08 15:15:47 -0500

    SASS lint must succeed before publishing to npm

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,14 +2,18 @@ require('shelljs/global')
 
 var argv = require('minimist')(process.argv.slice(2));
 
-const npmToken = argv.NPM_TOKEN
+// Validate SCSS
+exec('gulp lint:sass', exitCode => {
+  if (exitCode > 0) exit(1)
 
-// Create npm credentials
-const npmrc = `//registry.npmjs.org/:_authToken=${npmToken}`
-echo(npmrc).to('.npmrc')
+  // Create npm credentials
+  const npmToken = argv.NPM_TOKEN
+  const npmrc = `//registry.npmjs.org/:_authToken=${npmToken}`
+  echo(npmrc).to('.npmrc')
 
-// publish
-exec('npm publish source/styles')
+  // publish
+  exec('npm publish source/styles')
 
-// delete .npmrc
-rm('.npmrc')
+  // delete .npmrc
+  rm('.npmrc')
+})
